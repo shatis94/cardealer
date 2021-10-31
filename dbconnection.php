4b6e35d38d437488420e6dd9114fadb99de35c1d
unknown <Shatizshan@gmail.com>
2021-10-31 19:07:24 +0800

    deleted

--- dbconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db=mysqli_connect("localhost","root","","blog");
-if(!$db)
-{
-	die("could not connect".mysqli_error());
-}
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-session_start();
-unset($_SESSION['s_name']);
-session_destroy();
-header("location:index.php");
-
-?>
